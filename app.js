ea8409f98dab63d98dcc9ee924eb2da83b00a520
brandon-parkerson <brandon-m-parkerson@gmail.com>
2025-04-14 15:38:33 +0300

    feat: add a new message and be redirected with new message displayed

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// To parse data from the post request of a new message
+app.use(express.urlencoded({ extended: true }));
+
+
 // Run server
 app.listen(3005, () => {
     console.log("Listening on port 3005");
@@ -34,6 +38,17 @@ app.get("/", (req, res) => {
     res.render("index", {messages : messages});
 });
 
+// get new message req
 app.get("/new", (req, res) => {
-    res.send("new")
-})
+    res.render("form");
+});
+
+// post new message
+app.post("/new", (req, res) => {
+    // parse data from inputs
+    const name = req.body.name;
+    const message = req.body.message;
+    messages.push({ text : message, user : name, added : new Date() } );
+    // send user back to index
+    res.redirect('/');
+});
