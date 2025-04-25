68f03b487b857b602cef324ff5d5b855b9c4332d
brandon-parkerson <brandon-m-parkerson@gmail.com>
2025-04-25 17:18:38 +0300

    port

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true}));
 app.use("/", usersRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 8080;
 app.listen(PORT, () => console.log(`Express app listening on port: ${PORT}`));
 
 
