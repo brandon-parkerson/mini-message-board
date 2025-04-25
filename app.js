24b8505300a7825114637ba248e5efcfa5b239b6
brandon-parkerson <brandon-m-parkerson@gmail.com>
2025-04-25 18:53:35 +0300

    idk

--- app.js
+++ app.js
@@ -8,8 +8,18 @@ const usersRouter = require('./routes/usersRouter');
 
 
 app.set("view engine", "ejs");
+const path = require("path");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/", usersRouter);
+
+app.get("/ping", (req, res) => {
+    res.send("pong from Railway!");
+  });
+  
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Express app listening on port: ${PORT}`));
